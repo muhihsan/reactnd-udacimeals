0ecfccdc945ff04f5558a0843a52ab36881899e5
Muhammad Ihsan <mihsan191@gmail.com>
2017-09-11 15:54:02 +1000

    Fix style (tab)

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,11 @@ export const ADD_RECIPE = 'ADD_RECIPE';
 export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR';
 
 export const addRecipe = ({ day, recipe, meal }) => {
-    type: ADD_RECIPE,
-    recipe,
-    day,
-    meal
-  };
+  type: ADD_RECIPE,
+  recipe,
+  day,
+  meal
+};
 
 export const removeFromCalendar = ({ day, meal }) => {
   type: REMOVE_FROM_CALENDAR,
